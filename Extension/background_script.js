cad430ae11d9976dd7064d2c766a1b4acd168b74
VardanMittal <vardanmittal000@gmail.com>
2024-01-22 13:40:36 +0530

    "Adding some functions for geo locations"

--- Extension/background_script.js
+++ Extension/background_script.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
-const express = require("express");
+// const mongoose = require("mongoose");
+// const express = require("express");
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "data") {
-    const extractedData = message.data;
-
+    const extractedData = message.urls;
+    const location = message.location;
+    console.log("your location is: ", location);
     console.log("Received URLs from content script:", extractedData);
   }
 });
--- Extension/content_script.js
+++ Extension/content_script.js
@@ -1,20 +1,42 @@
+const data = {};
+
 function extractData() {
-  const data = {};
+  const urls = {};
   const linkElements = document.querySelectorAll("a.eVNpHGjtxRBq_gLOfGDr");
   const details = document.querySelectorAll("span.kY2IgmnCmOGjharHErah");
 
   const minLength = Math.min(linkElements.length, details.length);
 
   for (let i = 0; i < minLength; i++) {
-    data[i] = details[i].textContent;
+    urls[i] = details[i].textContent;
   }
 
-  return data;
+  data.urls = urls;
 }
 
-function sendUrlsToBackground(data) {
+function extractLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        data.location = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+      },
+      (error) => {
+        console.error("Error getting geolocation:", error.message);
+        return;
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported in this browser.");
+    return;
+  }
+}
+function sendUrlsToBackground() {
   browser.runtime.sendMessage({ action: "data", data });
 }
-
-const extractedData = extractData();
-sendUrlsToBackground(extractedData);
+console.log(data);
+extractData();
+extractLocation();
+sendUrlsToBackground();
